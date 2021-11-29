bb7ae0f6248173c6f32d534df786caaaedecd190
Edwin <skakodube@gmail.com>
2021-11-29 10:55:51 +0200

    added production loader

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require("./src/loaders/config")();
 const logger = require("./src/loaders/logging");
 require("./src/loaders/db")();
 require("./src/loaders/router")(app);
+require("./src/loaders/prod")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, logger.info(`Listening on port ${PORT}...`));
